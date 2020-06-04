b99ee5ac23d41065c1f9ff2fc4ef37b5662a425b
Yury Shipilov <yurshipilov@gmail.com>
2020-06-04 20:20:50 +0400

    Create the first model

--- news/views.py
+++ news/views.py
@@ -4,3 +4,7 @@ from django.http import HttpResponse
 def index(request):
     # print(request)
     return HttpResponse('Hello world')  
+
+def test(request):
+    return HttpResponse('<h1>Тестовая страница</h1>')
+ 
