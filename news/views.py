3634fd146473cff86a7cd867db8f68144fa8b332
Yury Shipilov <yurshipilov@gmail.com>
2020-07-06 22:23:51 +0400

    Creating forms

--- news/views.py
+++ news/views.py
@@ -1,5 +1,4 @@
-from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404, redirect
 
 from .models import News, Category
 from .forms import NewsForm
@@ -18,17 +17,21 @@ def get_category(request, category_id):
     news = News.objects.filter(category_id=category_id)
     category = Category.objects.get(pk=category_id)
     return render(request, 'news/category.html', {'news': news, 'category': category})
-    
+
 
 def view_news(request, news_id):
-   # news_item = News.objects.get(pk=news_id)
-    news_item = get_object_or_404(News, pk = news_id)
-    return render(request, 'news/view_news.html', {'news_item': news_item})
+    # news_item = News.objects.get(pk=news_id)
+    news_item = get_object_or_404(News, pk=news_id)
+    return render(request, 'news/view_news.html', {"news_item": news_item})
 
 
 def add_news(request):
     if request.method == 'POST':
-        pass
+        form = NewsForm(request.POST)
+        if form.is_valid():
+            # print(form.cleaned_data)
+            news = News.objects.create(**form.cleaned_data)
+            return redirect(news)
     else:
         form = NewsForm()
     return render(request, 'news/add_news.html', {'form': form})
